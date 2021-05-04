bfe19a340d3277990a5bfa3b8c934347c16cf9b5
sinethvip <83576672+sinethvip@users.noreply.github.com>
2021-05-04 09:46:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,19 +16,19 @@ try:
     apihash = config("API_HASH")
     bottoken = config("BOT_TOKEN")
     FRWD_CHANNEL = config("FRWD_CHANNEL", cast=int)
-    BotzHub = TelegramClient('BotzHub', apiid, apihash).start(bot_token=bottoken)
+    BotzHub = TelegramClient('sl Bot zone', apiid, apihash).start(bot_token=bottoken)
 except:
     print("Environment vars are missing! Kindly recheck.")
     print("Bot is quiting...")
     exit()
 
-@BotzHub.on(events.NewMessage(pattern="/start", func=lambda e: e.is_private))
+@Botbot zone.on(events.NewMessage(pattern="/start", func=lambda e: e.is_private))
 async def _(event):
-    ok = await BotzHub(GetFullUserRequest(event.sender_id))
+    ok = await sl Bot zone(GetFullUserRequest(event.sender_id))
     await event.reply(f"Hello {ok.user.first_name}! \nI'm a view-counter bot.\nSend me a message and I'll attach a view count to it!",
                     buttons=[
-                        [Button.url("Dev.", url="https://t.me/BotzHub"),
-                        Button.url("Repository", url="https://github.com/xditya/ViewCountBot")]
+                        [Button.url("sl bot zone", url="https://t.me/slbotzone"
+     
                     ])
 
 @BotzHub.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
@@ -39,5 +39,5 @@ async def countit(event):
     await x.forward_to(event.chat_id)
 
 print("Bot has started.")
-print("Do visit @BotzHub..")
+print("Do visit @slbotzone")
 BotzHub.run_until_disconnected()
